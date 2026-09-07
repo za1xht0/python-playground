d37c3fa3058da055dc136e4c51d47319dac6d334
Egor <raccooncool437@gmail.com>
2026-09-07 21:31:41 +0300

    fixing names

--- password_generator/pass.py
+++ password_generator/pass.py
@@ -4,14 +4,14 @@ print('Это генератор паролей.\nОн генерирует сл
 numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 symbols = ["!", '@', '#', '$']
-ls = []
-q1 = ''
-while q1 != 'закончить':
+password = []
+stop_word = ''
+while stop_word != 'закончить':
     for i in range(0, 4):
-        ls.append(random.choice(numbers))
-        ls.append(random.choice(letters))
-        ls.append(random.choice(symbols))
-    random.shuffle(ls)
-    print('Вот ваш пароль: ', *ls, sep='')
-    ls = []
-    q1 = input('Если хочешь закончить, напиши - закончить\nЕсли продолжаем - напиши продолжаем\n')
+        password.append(random.choice(numbers))
+        password.append(random.choice(letters))
+        password.append(random.choice(symbols))
+    random.shuffle(password)
+    print('Вот ваш пароль: ', *password, sep='')
+    password = []
+    stop_word = input('Если хочешь закончить, напиши - закончить\nЕсли продолжаем - напиши продолжаем\n')
