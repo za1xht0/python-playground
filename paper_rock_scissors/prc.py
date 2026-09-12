dcde41a0d92fa0ea5844308200ff494375b7bc94
Egor Mac <avsyukove@gamil.com>
2026-09-12 13:07:28 +0300

    minor changes in rock paper scissors

--- paper_rock_scissors/prc.py
+++ paper_rock_scissors/prc.py
@@ -2,26 +2,34 @@ import random
 
 print('Игра камень ножницы бумага')
 
+status = False
 choices = ['Камень', 'Ножницы', 'Бумага']
 raw = ''
-while raw not in ['1', '2', '3']:
-    raw = input('Выберите:\n1. Камень\n2. Ножницы\n3. Бумага\n(Нужно ввести число)\n')
-    try:
-        player = int(raw)
-        if player < 1 or player > 3:
-            print('Нужно ввести число от 1 до 3')
-            player = -1
-    except ValueError:
-        print('Нужно ввести число')
-        player = -1
+while status == False:
+    status_check = input(('Играем?\n 1. Да\n 2. Нет\n'))
+    if status_check == '1':
+        while raw not in ['1', '2', '3']:
+            raw = input('Выберите:\n1. Камень\n2. Ножницы\n3. Бумага\n(Нужно ввести число)\n')
+            try:
+                player = int(raw)
+                if player < 1 or player > 3:
+                    print('Нужно ввести число от 1 до 3')
+                    player = -1
+            except ValueError:
+                print('Нужно ввести число')
+                player = -1
 
-pl = choices[player - 1]
-pc_answer = random.choice(choices)
-print(pl, pc_answer)
+        pl = choices[player - 1]
+        pc_answer = random.choice(choices)
+        print(f'Ты выбрал: {pl}, у меня было: {pc_answer}')
 
-if pl == 'Камень' and pc_answer == 'Бумага' or pl == 'Ножницы' and pc_answer == 'Камень' or pl == 'Бумага' and pc_answer == 'Ножницы':
-    print("Ты проиграл")
-elif pl == pc_answer:
-    print('Ничья')
-else:
-    print("Ты выиграл")
+        if pl == 'Камень' and pc_answer == 'Бумага' or pl == 'Ножницы' and pc_answer == 'Камень' or pl == 'Бумага' and pc_answer == 'Ножницы':
+            print("Ты проиграл")
+        elif pl == pc_answer:
+            print('Ничья')
+        else:
+            print("Ты выиграл")
+    elif status_check == '2':
+        status = True
+        print('Конец игры')
+        break
