08076032f4335ec6a5e5566833d1e975a6afe55a
Egor <raccooncool437@gmail.com>
2026-09-20 20:55:00 +0300

    add os check, fixing get_exit_code and overall_status

--- devops/server_health_checker/shc.py
+++ devops/server_health_checker/shc.py
@@ -1,5 +1,6 @@
 import sys
 import psutil
+import platform
 
 print('Server Health Checker\n')
 is_on = int(input('Начинаем?\n1. Да\n2. Нет\n'))
@@ -7,6 +8,16 @@ cpu_ram_ok = 80
 cpu_ram_warning = 90
 disk_ok = 90
 disk_warning = 95
+
+if platform.system() == 'Linux':
+    disk_path = '/home'
+elif platform.system() == 'Windows':
+    disk_path = 'C:\\'
+elif platform.system() == 'Darwin':
+    disk_path = '/Users'
+else:
+    print('Unsupported operating system')
+    sys.exit(1)
 
 def cpu_check(cpu):
     if cpu < cpu_ram_ok:
@@ -32,13 +43,15 @@ def disk_check(disk):
     else:
         return 'CRITICAL'
 
-def service_status(cpu_result, ram_result, disk_result):
+def overall_status(cpu_result, ram_result, disk_result):
     if cpu_result == 'OK' and ram_result == 'OK' and disk_result == 'OK':
         return 'OK'
     elif cpu_result == 'CRITICAL' or ram_result == 'CRITICAL' or disk_result == 'CRITICAL' :
         return 'CRITICAL'
     elif cpu_result == 'WARNING' or ram_result == 'WARNING' or disk_result == 'WARNING' :
         return 'WARNING'
+    else:
+        return 'UNKNOWN'
     
 def get_exit_code(status):
     if status == 'OK':
@@ -47,12 +60,14 @@ def get_exit_code(status):
         return 1
     elif status == 'CRITICAL':
         return 2
+    elif status == 'UNKNOWN':
+        return 3
 
 def main(cpu, ram, disk):
     cpu_result = cpu_check(cpu)
     ram_result =  ram_check(ram)
     disk_result =  disk_check(disk)
-    serv = service_status(cpu_result, ram_result, disk_result)
+    serv = overall_status(cpu_result, ram_result, disk_result)
     exit_code = get_exit_code(serv)
     return f'\nServer Health Checker\n\nCPU: {cpu}% ---- {cpu_result}\nMemory: {ram}% ---- {ram_result}\nDisk: {disk}% ---- {disk_result}\n\nOverall status: {serv}\n', exit_code
 exit_code = 0
@@ -61,7 +76,7 @@ while True:
         break
     cpu = psutil.cpu_percent(interval=1)
     ram = psutil.virtual_memory().percent
-    disk = psutil.disk_usage('/home').percent
+    disk = psutil.disk_usage(disk_path).percent
     res, exit_code = main(cpu, ram, disk)
     print(res)
     is_on = int(input('Хотите проверить еще раз?\n1. Да\n2. Нет\n'))
